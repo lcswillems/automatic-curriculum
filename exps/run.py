bcc7d7076b2403e76ba028db44197d1a5a557403
Lucas Willems <lcswillems@gmail.com>
2018-06-03 23:36:23 -0400

    Defined experiences

---
+++ exps/run.py
@@ -0,0 +1,24 @@
+import subprocess
+import time
+import itertools
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--no-cluster", action="store_true", default=False)
+args = parser.parse_args()
+
+seeds = range(1, 4)
+graphs = ["SC-Edgeless", "BabyAI-BlockedUnlockPickup"]
+lp_cps = ["Online", "AbsOnline", "Window", "AbsWindow", "AbsLinreg"]
+dist_cps = ["GreedyAmax", "GreedyProp", "ClippedProp", "Boltzmann"]
+dist_aut_upds = [True, False]
+
+for seed, graph, lp_cp, dist_cp, dist_aut_upd in itertools.product(seeds, graphs, lp_cps, dist_cps, dist_aut_upds):
+    model_name = "{}_{}_{}_{}/seed{}".format(graph, lp_cp, dist_cp, dist_aut_upd, seed)
+    subprocess.Popen(
+        "{} exps/run.sh python -m scripts.train --seed {} --graph {} --lp {} --dist {} {} --save-interval 10 --procs 1 --frames-per-proc 2048 --frames 3000"
+        .format("sbatch --account=def-bengioy --time=0:20:0 --ntasks=1" if not args.no_cluster else "",
+                seed, graph, lp_cp, dist_cp,
+                "--dist-automatic-update" if dist_aut_upd else ""),
+        shell=True)
+    time.sleep(1)
