f80bc9f992fd0c4dd9b67c861a99e531666e5569
Lucas Willems <lcswillems@gmail.com>
2018-06-06 03:17:53 -0400

    Added new graphs

--- graphs.py
+++ graphs.py
@@ -34,3 +34,27 @@ G.add_edges_from([
     ("BabyAI-UnlockPickup-v0", "BabyAI-BlockedUnlockPickup-v0")
 ])
 graphs["BabyAI-BlockedUnlockPickup"] = G
+
+G = nx.Graph()
+G.add_edges_from([
+    ("BabyAI-KeyCorridorS3R1-v0", "BabyAI-KeyCorridorS3R2-v0"),
+    ("BabyAI-KeyCorridorS3R2-v0", "BabyAI-KeyCorridorS3R3-v0"),
+    ("BabyAI-KeyCorridorS3R3-v0", "BabyAI-KeyCorridorS4R3-v0"),
+    ("BabyAI-KeyCorridorS4R3-v0", "BabyAI-KeyCorridorS5R3-v0"),
+    ("BabyAI-KeyCorridorS5R3-v0", "BabyAI-KeyCorridorS6R3-v0")
+])
+graphs["BabyAI-KeyCorridor"] = G
+
+G = nx.Graph()
+G.add_edges_from([
+    ("BabyAI-FindObjS5-v0", "BabyAI-FindObjS6-v0"),
+    ("BabyAI-FindObjS6-v0", "BabyAI-FindObjS7-v0")
+])
+graphs["BabyAI-FindObj"] = G
+
+G = nx.Graph()
+G.add_edges_from([
+    ("BabyAI-FourObjsS5-v0", "BabyAI-FourObjsS6-v0"),
+    ("BabyAI-FourObjsS6-v0", "BabyAI-FourObjsS7-v0")
+])
+graphs["BabyAI-FourObjs"] = G
