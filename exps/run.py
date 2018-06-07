3e59a6413066a85b5ad7a4ef7cfd84ff828f6d17
Lucas Willems <lcswillems@gmail.com>
2018-06-07 16:50:30 -0400

    Removed `automatic_updates`

--- exps/run.py
+++ exps/run.py
@@ -30,10 +30,6 @@ dist_cps = [
     # "Boltzmann",
     # "GreedyAmax"
 ]
-dist_aut_upds = [
-    False
-    # True,
-]
 εs = [
     0.1,
     0.2,
@@ -47,12 +43,11 @@ Ks = [
     100
 ]
 
-for seed, graph, lp_cp, dist_cp, dist_aut_upd, ε, K in itertools.product(seeds, graphs, lp_cps, dist_cps, dist_aut_upds, εs, Ks):
-    model_name = "{}_{}_{}_{}/seed{}".format(graph, lp_cp, dist_cp, dist_aut_upd, seed)
+for seed, graph, lp_cp, dist_cp, ε, K in itertools.product(seeds, graphs, lp_cps, dist_cps, εs, Ks):
+    model_name = "{}_{}_{}_eps{}_K{}/seed{}".format(graph, lp_cp, dist_cp, ε, K, seed)
     subprocess.Popen(
-        "{} exps/run.sh python -m scripts.train --seed {} --graph {} --lp {} --lp-K {} --dist {} {} --dist-eps {} --model {} --save-interval 10 --procs 1 --frames-per-proc 2048"
+        "{} exps/run.sh python -m scripts.train --seed {} --graph {} --lp {} --lp-K {} --dist {} --dist-eps {} --model {} --save-interval 10 --procs 1 --frames-per-proc 2048"
         .format("sbatch --account=def-bengioy --time=3:0:0 --ntasks=1" if not args.no_cluster else "",
-                seed, graph, lp_cp, ε, dist_cp, "--dist-automatic-update" if dist_aut_upd else "", K,
-                model_name),
+                seed, graph, lp_cp, ε, dist_cp, K, model_name),
         shell=True)
     time.sleep(1)
