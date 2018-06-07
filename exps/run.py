9ff8533a11c72e8badf1e6e323900b6c9e92abdf
Lucas Willems <lcswillems@gmail.com>
2018-06-07 16:48:16 -0400

    Updated experiments

--- exps/run.py
+++ exps/run.py
@@ -8,18 +8,51 @@ parser.add_argument("--no-cluster", action="store_true", default=False)
 args = parser.parse_args()
 
 seeds = range(1, 4)
-graphs = ["SC-Edgeless", "BabyAI-BlockedUnlockPickup"]
-lp_cps = ["Online", "AbsOnline", "Window", "AbsWindow", "AbsLinreg"]
-dist_cps = ["GreedyAmax", "GreedyProp", "ClippedProp", "Boltzmann"]
-dist_aut_upds = [True, False]
+graphs = [
+    # "SC-Edgeless",
+    # "SC-Normal",
+    "BabyAI-BlockedUnlockPickup",
+    "BabyAI-UnlockPickupDist",
+    "BabyAI-FourObjs",
+    # "BabyAI-FindObj",
+    # "BabyAI-KeyCorridor"
+]
+lp_cps = [
+    "Linreg",
+    # "Window",
+    # "AbsWindow",
+    # "Online",
+    # "AbsOnline"
+]
+dist_cps = [
+    "GreedyProp",
+    # "ClippedProp",
+    # "Boltzmann",
+    # "GreedyAmax"
+]
+dist_aut_upds = [
+    False
+    # True,
+]
+εs = [
+    0.1,
+    0.2,
+    0.5,
+    1
+]
+Ks = [
+    10,
+    20,
+    50,
+    100
+]
 
-for seed, graph, lp_cp, dist_cp, dist_aut_upd in itertools.product(seeds, graphs, lp_cps, dist_cps, dist_aut_upds):
+for seed, graph, lp_cp, dist_cp, dist_aut_upd, ε, K in itertools.product(seeds, graphs, lp_cps, dist_cps, dist_aut_upds, εs, Ks):
     model_name = "{}_{}_{}_{}/seed{}".format(graph, lp_cp, dist_cp, dist_aut_upd, seed)
     subprocess.Popen(
-        "{} exps/run.sh python -m scripts.train --seed {} --graph {} --lp {} --dist {} {} --model {} {} --save-interval 10 --procs 1 --frames-per-proc 2048"
+        "{} exps/run.sh python -m scripts.train --seed {} --graph {} --lp {} --lp-K {} --dist {} {} --dist-eps {} --model {} --save-interval 10 --procs 1 --frames-per-proc 2048"
         .format("sbatch --account=def-bengioy --time=3:0:0 --ntasks=1" if not args.no_cluster else "",
-                seed, graph, lp_cp, dist_cp, "--dist-automatic-update" if dist_aut_upd else "",
-                model_name,
-                "--no-instr --no-mem" if graph == "BabyAI-BlockedUnlockPickup" else ""),
+                seed, graph, lp_cp, ε, dist_cp, "--dist-automatic-update" if dist_aut_upd else "", K,
+                model_name),
         shell=True)
     time.sleep(1)
