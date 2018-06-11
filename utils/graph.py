ef2c4668b7ad6502c4dde3768e8cb6d747dcb642
Lucas Willems <lcswillems@gmail.com>
2018-06-10 23:20:51 -0400

    Simplified code

--- utils/graph.py
+++ utils/graph.py
@@ -5,9 +5,7 @@ import utils
 
 def make_envs_graph(graph_id, seed):
     G = graphs.graphs[graph_id]
-    mapping = {}
-    for env_id in G.nodes:
-        mapping[env_id] = utils.make_env(env_id, seed)
+    mapping = {env_id: utils.make_env(env_id, seed) for env_id in G.nodes}
     return nx.relabel_nodes(G, mapping)
 
 def get_graph_env_ids(graph_id):
