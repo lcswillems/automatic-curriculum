21624a97f2f2553ffc8d83b8d5d2bdf8e379e9ed
Lucas Willems <lcswillems@gmail.com>
2018-06-19 01:40:58 -0400

    Compute Lppot with multiplication instead of addition and normalized progress

--- menv/pot_cp.py
+++ menv/pot_cp.py
@@ -23,23 +23,25 @@ class RwpotPotComputer(PotComputer):
         super().__init__(num_envs)
 
         self.K = K
-        self.min_returns = [float("+inf")]*self.num_envs if min_returns is None else min_returns
-        self.max_returns = [float("-inf")]*self.num_envs if max_returns is None else max_returns
+        self.min_returns = numpy.full(self.num_envs, numpy.inf) if min_returns is None else numpy.array(min_returns)
+        self.max_returns = numpy.full(self.num_envs, -numpy.inf) if max_returns is None else numpy.array(max_returns)
 
         self.rwpots = numpy.zeros((self.num_envs))
         self.pots = self.rwpots
 
         for env_id in range(len(self.rwpots)):
-            min_return = self.min_returns[env_id]
-            max_return = self.max_returns[env_id]
-            self.rwpots[env_id] = max(max_return - min_return, 0)
+            RwpotPotComputer._compute_pot(self, env_id)
 
     def _compute_pot(self, env_id):
         returns = self.returns[env_id][-self.K:]
-        returnn = numpy.mean(returns)
-        min_return = min(self.min_returns[env_id], returnn)
-        max_return = max(self.max_returns[env_id], returnn)
-        self.rwpots[env_id] = max_return - returnn
+        min_return = self.min_returns[env_id]
+        max_return = self.max_returns[env_id]
+        returnn = min_return
+        if len(returns) > 0:
+            min_return = min(min_return, returnn)
+            max_return = max(max_return, returnn)
+            returnn = numpy.mean(returns)
+        self.rwpots[env_id] = max(max_return - returnn, 0)
         if len(returns) >= self.K:
             self.min_returns[env_id] = min_return
             self.max_returns[env_id] = max_return
@@ -53,9 +55,16 @@ class LppotPotComputer(RwpotPotComputer):
         self.lppots = numpy.zeros((self.num_envs))
         self.pots = self.lppots
 
+        for env_id in range(len(self.lppots)):
+            self._compute_pot(env_id)
+
     def _compute_pot(self, env_id):
         super()._compute_pot(env_id)
 
         predecessors = list(self.G.predecessors(env_id))
-        predecessors_rwpot = numpy.mean(self.rwpots[predecessors]) if len(predecessors) > 0 else 0
-        self.lppots[env_id] = self.rwpots[env_id] - predecessors_rwpot
+        if len(predecessors) > 0:
+            predecessors_norm = self.max_returns[predecessors] - self.min_returns[predecessors]
+            predecessors_pot = numpy.mean(self.rwpots[predecessors] / predecessors_norm)
+        else:
+            predecessors_pot = 0
+        self.lppots[env_id] = self.rwpots[env_id] * (1 - predecessors_pot)
