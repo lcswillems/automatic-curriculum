7dfa527e5c0b511fba3117227c3b342b4b7d4b43
Lucas Willems <lcswillems@gmail.com>
2018-06-22 19:10:18 -0400

    Fixed bug

--- menv/dist_cp.py
+++ menv/dist_cp.py
@@ -3,18 +3,29 @@ import numpy
 import networkx as nx
 
 class DistComputer(ABC):
+    def __init__(self, return_hists):
+        self.return_hists = return_hists
+
+        self.step = 0
+
     @abstractmethod
     def __call__(self, returns):
-        pass
+        self.step += 1
+        for env_id, returnn in returns.items():
+            self.return_hists[env_id].append(self.step, returnn)
 
 class LpDistComputer(DistComputer):
     def __init__(self, return_hists, compute_lp, create_dist):
+        super().__init__(return_hists)
+
         self.return_hists = return_hists
         self.compute_lp = compute_lp
         self.create_dist = create_dist
 
     def __call__(self, returns):
-        self.lps = self.compute_lp(returns)
+        super().__call__(returns)
+
+        self.lps = self.compute_lp()
         self.attentions = numpy.absolute(self.lps)
         dist = self.create_dist(self.attentions)
 
@@ -22,7 +33,8 @@ class LpDistComputer(DistComputer):
 
 class LpPotDistComputer(DistComputer):
     def __init__(self, return_hists, compute_lp, compute_pot, create_dist, pot_coef):
-        self.return_hists = return_hists
+        super().__init__(return_hists)
+
         self.compute_lp = compute_lp
         self.compute_pot = compute_pot
         self.create_dist = create_dist
@@ -31,9 +43,7 @@ class LpPotDistComputer(DistComputer):
         self.step = 0
 
     def __call__(self, returns):
-        self.step += 1
-        for env_id, returnn in returns.items():
-            self.return_hists[env_id].append(self.step, returnn)
+        super().__call__(returns)
 
         self.lps = self.compute_lp()
         self.pots = self.compute_pot()
