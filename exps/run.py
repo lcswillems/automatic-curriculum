de581bbe9c60f1cdb2cfd505d4e9202ab299bfbd
Lucas Willems <lcswillems@gmail.com>
2018-09-05 18:00:16 +0200

    Simplified experiments

--- exps/run.py
+++ exps/run.py
@@ -27,23 +27,11 @@ lp_cps = [
     # "Window",
     # "Online",
 ]
-dist_crs = [
+dist_cvs = [
     "Prop",
     # "GreedyProp",
     # "Boltzmann",
     # "GreedyAmax"
-]
-Ks = [
-    10,
-    # 20,
-    # 50,
-    # 100
-]
-εs = [
-    0.1,
-    # 0.2,
-    # 0.5,
-    # 1
 ]
 pot_props = [
     0,
@@ -58,21 +46,15 @@ times = {
     "KeyCorridor": "2:0:0",
     "ObstructedMaze": "3:0:0",
 }
-no_comps = {
-    "BlockedUnlockPickup": "--no-instr",
-    "KeyCorridor": "--no-instr",
-    "ObstructedMaze": "--no-instr",
-}
 
 # Execute scripts
 
-for seed, curriculum, dist_cp, lp_cp, dist_cr, K, ε, pot_prop in itertools.product(seeds, curriculums, dist_cps, lp_cps, dist_crs, Ks, εs, pot_props):
+for seed, curriculum, dist_cp, lp_cp, dist_cv, pot_prop in itertools.product(seeds, curriculums, dist_cps, lp_cps, dist_cvs, pot_props):
     slurm_cmd = "sbatch --account=def-bengioy --time={} --cpus-per-task=4 --gres=gpu:1 --mem=4G".format(times[curriculum])
-    model_name = "{}_{}_{}_{}_K{}_eps{}_prop{}/seed{}".format(curriculum, dist_cp, lp_cp, dist_cr, K, ε, pot_prop, seed)
-    no_comp = no_comps[curriculum]
+    model_name = "{}_{}_{}_{}_prop{}/seed{}".format(curriculum, dist_cp, lp_cp, dist_cv, pot_prop, seed)
     subprocess.Popen(
-        "{} exps/run.sh python -m scripts.train --seed {} --curriculum {} --dist-cp {} --lp-cp {} --dist-cr {} --dist-lp-K {} --dist-eps {} --pot-prop {} --model {} {} --save-interval 10"
+        "{} exps/run.sh python -m scripts.train --seed {} --curriculum {} --dist-cp {} --lp-cp {} --dist-cr {} --pot-prop {} --model {} --save-interval 10"
         .format(slurm_cmd if not args.no_slurm else "",
-                seed, curriculum, dist_cp, lp_cp, dist_cr, K, ε, pot_prop, model_name, no_comp),
+                seed, curriculum, dist_cp, lp_cp, dist_cv, pot_prop, model_name),
         shell=True)
     time.sleep(1)
