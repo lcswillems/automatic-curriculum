d04f04e01a32820abf28ada75760ee1b65138b01
Lucas Willems <lcswillems@gmail.com>
2018-09-06 02:10:26 +0200

    Reordering and deletion of useless lines

--- multienv/__init__.py
+++ multienv/__init__.py
@@ -1,5 +1,5 @@
 from .dist_computer import *
 from .dist_converter import *
 from .lp_computer import *
-from .return_history import *
 from .multienv import MultiEnvHead, MultiEnv
+from .return_history import *
