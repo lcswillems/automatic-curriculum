5230c0d260b6a211c5380d3d906abe251d1120ce
saleml <salemlahlou9@gmail.com>
2019-06-17 14:30:42 -0400

    Data generator

--- utils/general.py
+++ utils/general.py
@@ -2,23 +2,31 @@ import random
 import os
 import numpy
 import torch
-import collections
+
 
 def get_storage_dir():
     if "SOPH_CURRI_STORAGE" in os.environ:
         return os.environ["SOPH_CURRI_STORAGE"]
     return "storage"
 
+
 def get_model_dir(model_name):
     return os.path.join(get_storage_dir(), "models", model_name, "")
 
+
+def get_csv_config_path():
+    return os.path.join(get_storage_dir(), "config.csv")
+
+
 def get_stats_dir():
     return os.path.join(get_storage_dir(), "stats", "")
+
 
 def create_folders_if_necessary(path):
     dirname = os.path.dirname(path)
     if not os.path.isdir(dirname):
         os.makedirs(dirname)
+
 
 def seed(seed):
     random.seed(seed)
