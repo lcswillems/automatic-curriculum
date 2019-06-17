6e4d34f725eef18106ac599737100cc518a0f28d
saleml <salemlahlou9@gmail.com>
2019-06-17 14:31:26 -0400

    Add LSTM model

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
 import torch_ac
 
+
 # Function from https://github.com/ikostrikov/pytorch-a2c-ppo-acktr/blob/master/model.py
 def initialize_parameters(m):
     classname = m.__class__.__name__
@@ -12,6 +13,7 @@ def initialize_parameters(m):
         m.weight.data *= 1 / torch.sqrt(m.weight.data.pow(2).sum(1, keepdim=True))
         if m.bias is not None:
             m.bias.data.fill_(0)
+
 
 class ACModel(nn.Module, torch_ac.ACModel):
     def __init__(self, obs_space, action_space):
@@ -60,3 +62,76 @@ class ACModel(nn.Module, torch_ac.ACModel):
         value = x.squeeze(1)
 
         return dist, value
+
+
+class AdditionEncoderRNN(nn.Module):
+    def __init__(self, input_size=11, hidden_size=128, device=None):
+        super(AdditionEncoderRNN, self).__init__()
+
+        if device is None:
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = device
+
+        self.hidden_size = hidden_size
+
+        self.lstm = nn.LSTM(input_size, hidden_size)
+
+    def forward(self, input, hidden):
+        # input has to be of shape (seq_len=19, batch, input_size=10)
+        # hidden is a tuple with both elements of shape (1, batch, hidden_size=128)
+        output, hidden = self.lstm(input, hidden)
+        return output, hidden
+
+    def initHidden(self, batch_size=4096):
+        return (torch.zeros(1, batch_size, self.hidden_size, device=self.device),
+                torch.zeros(1, batch_size, self.hidden_size, device=self.device))
+
+
+class AdditionDecoderRNN(nn.Module):
+    def __init__(self, hidden_size=128, output_size=10, seq_len=10, device=None):
+        super(AdditionDecoderRNN, self).__init__()
+
+        if device is None:
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = device
+
+        self.seq_len = seq_len
+
+        self.hidden_size = hidden_size
+
+        self.lstm = nn.LSTM(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=2)
+
+    def forward(self, input, hidden):
+        # input should be the encoder's output[-1] of shape (batch_size, hidden_size)
+        output = input.repeat(self.seq_len, 1, 1)
+        output, hidden = self.lstm(output, hidden)
+        output = self.softmax(self.out(output))
+        # output should be of shape (seq_len=10, batch_size, output_size=10) with values being log-probabilities
+        return output
+
+    def initHidden(self, batch_size=4096):
+        return (torch.zeros(1, batch_size, self.hidden_size, device=self.device),
+                torch.zeros(1, batch_size, self.hidden_size, device=self.device))
+
+
+class AdditionModel(nn.Module):
+    def __init__(self, input_size=11, hidden_size=128, output_size=10, output_seq_len=10, device=None):
+        super(AdditionModel, self).__init__()
+        self.encoder = AdditionEncoderRNN(input_size, hidden_size, device)
+        self.decoder = AdditionDecoderRNN(hidden_size, output_size, output_seq_len, device)
+
+    def forward(self, input):
+        # input has to be of shape (seq_len=19, batch, input_size=11)
+        batch_size = input.size(1)
+        hidden = self.encoder.initHidden(batch_size)
+
+        encoder_output, _ = self.encoder(input, hidden)
+        decoder_input = encoder_output[-1]
+
+        hidden = self.decoder.initHidden(batch_size)
+        decoder_output = self.decoder(decoder_input, hidden)
+
+        # decoder_output is of shape(seq_len=10, batch, output_size=10)
+        return decoder_output
