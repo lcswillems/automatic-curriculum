cf847cb7f73d99b6c25666bc9f40dd324fb37add
saleml <salemlahlou9@gmail.com>
2019-06-18 12:52:44 -0400

    trivial commit

--- polyenv/__init__.py
+++ polyenv/__init__.py
@@ -1,5 +1,5 @@
 from .dist_computer import *
 from .dist_converter import *
 from .lp_estimator import *
-from .polyenv import PolyEnvHead, PolyEnv
+from .polyenv import PolyEnvHead, PolyEnv, PolySupervisedEnv
 from .return_history import *
