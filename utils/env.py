1cbc49d4b640aef9fbbbc1aad82a02f2fd52fbc8
saleml <salemlahlou9@gmail.com>
2019-06-18 12:51:22 -0400

    make an addition environment, this doesn't work I think because I'm importing addition_env ---> maybe make a package

--- utils/env.py
+++ utils/env.py
@@ -1,7 +1,14 @@
 import gym
 import gym_minigrid
+import addition_env
+
 
 def make_env(env_key, seed):
     env = gym.make(env_key)
     env.seed(seed)
+    return env
+
+
+def make_addition_env(seq_len, number_of_digits, seed):
+    env = addition_env.AdditionEnvironment(seq_len, number_of_digits, seed)
     return env
